96f6329880937776000cdef18a2b5ea9b554ecbb
bogom <bogomolov222@gmail.com>
2020-10-30 06:02:22 +0400

    добавлены файлы миграции. добавлены классы на сущности студент, курс, специальность, факультет, группа добавлен контроллер на обработку всех моделей

--- deanery/routes/web.php
+++ deanery/routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'Maincontroller@homePage');
+
+Route::get('/courses', 'Maincontroller@coursesPage');
+
+Route::get('/faculties', 'Maincontroller@facultiesPage');
+
+Route::get('/majors', 'Maincontroller@majorsPage');
+
+Route::get('/groups', 'Maincontroller@groupsPage');
+
+Route::get('/students', 'Maincontroller@studentsPage');
+
+Route::get('/student', 'Maincontroller@studentPage');
+
+
+
